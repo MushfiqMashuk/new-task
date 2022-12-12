387eb3e5fdc369bf5ad676267294f5525e97c987
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2022-12-12 19:17:42 +0600

    Pagination component

---
+++ components/Pagination/index.jsx
@@ -0,0 +1,80 @@
+import { DOTS, usePagination } from "../../hooks/usePagination";
+import styles from "./pagination.module.scss";
+const Pagination = (props) => {
+  const {
+    onPageChange,
+    totalCount,
+    siblingCount = 1,
+    currentPage,
+    pageSize,
+    className,
+  } = props;
+
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize,
+  });
+
+  // If there are less than 2 times in pagination range we shall not render the component
+  if (currentPage === 0 || paginationRange.length < 2) {
+    return null;
+  }
+
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
+
+  let lastPage = paginationRange[paginationRange.length - 1];
+  return (
+    <ul className={`${styles.pagination_container} ${className && className}`}>
+      {/* Left navigation arrow */}
+      <li
+        className={`${styles.pagination_item} ${
+          currentPage === 1 ? `${styles.disabled}` : ""
+        }`}
+        onClick={onPrevious}
+      >
+        <div className={`${styles.arrow} ${styles.left}`} />
+      </li>
+      {paginationRange.map((pageNumber) => {
+        // If the pageItem is a DOT, render the DOTS unicode character
+        if (pageNumber === DOTS) {
+          return (
+            <li className={`${styles.pagination_item} ${styles.dots}`}>
+              &#8230;
+            </li>
+          );
+        }
+
+        // Render our Page Pills
+        return (
+          <li
+            className={`${styles.pagination_item} ${
+              pageNumber === currentPage ? `${styles.selected}` : ""
+            }`}
+            onClick={() => onPageChange(pageNumber)}
+          >
+            {pageNumber}
+          </li>
+        );
+      })}
+      {/*  Right Navigation arrow */}
+      <li
+        className={`${styles.pagination_item} ${
+          currentPage === lastPage ? `${styles.disabled}` : ""
+        }`}
+        onClick={onNext}
+      >
+        <div className={`${styles.arrow} ${styles.right}`} />
+      </li>
+    </ul>
+  );
+};
+
+export default Pagination;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
+import Modal from "../components/Modal";
 import Tabs from "../components/Tabs";
 import styles from "./home.module.scss";
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <Layout>
       <div className={styles.container}>
-        <Tabs>
+        {showModal && (
+          <Modal title="Add new user" onClose={() => setShowModal(false)}>
+            Nice looing Kid
+          </Modal>
+        )}
+        <div className={styles.add_user}>
+          <button
+            className={styles.add_button}
+            onClick={() => setShowModal(true)}
+          >
+            Add User
+          </button>
+        </div>
+        <Tabs title="User Info">
           <div label="Admin" onClick={() => console.log("Nice")}>
             I am an admin
           </div>
